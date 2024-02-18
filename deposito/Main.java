48b5dd8485d255ddefdf5dc1a2aee130b1fbbe6f
dvillanuevar01 <dvillanuevar01@educastillalamancha.es>
2024-02-18 21:00:42 +0100

    Inserta comentarios Javadoc en la clase Ccuenta

--- deposito/Main.java
+++ deposito/Main.java
@@ -7,18 +7,19 @@ public class Main {
         double saldoActual;
         float ingreso;
         float retirada;
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+            saldoActual = cuenta1.estado();
+            System.out.println("El saldo actual es"+ cuenta1.getSaldo() );
+            System.out.println("Introduce la cantidad que quieres ingresar");
+            ingreso = sc.nextFloat(); sc.nextLine();
+            System.out.println("Introduce la cantidad que quieres retirar");
+            retirada = sc.nextFloat(); sc.nextLine();
+        }
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ cuenta1.getSaldo() );
-        System.out.println("Introduce la cantidad que quieres ingresar");
-        ingreso = sc.nextFloat(); sc.nextLine();
-        System.out.println("Introduce la cantidad que quieres retirar");
-        retirada = sc.nextFloat(); sc.nextLine();
         operativa_cuenta(cuenta1,ingreso,retirada);
     }
-    
+
         public static void operativa_cuenta(CCuenta cuenta,float ingresar, float retirar) {
         try {
             cuenta.retirar(retirar);
