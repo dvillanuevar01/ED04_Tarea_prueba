2ac388fe73263b9b22ee3e2653415f203715decb
dvillanuevar01 <dvillanuevar01@educastillalamancha.es>
2024-02-18 20:40:57 +0100

    Añadir un nuevo parámetro al método operativa_cuenta, de nombre cantidad y de tipo float

--- deposito/Main.java
+++ deposito/Main.java
@@ -1,27 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
+        float ingreso;
+        float retirada;
+        Scanner sc = new Scanner(System.in);
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
-
-        operativa_cuenta(cuenta1);
-
-        public static void operativa_cuenta(CCuenta cuenta) {
+        System.out.println("El saldo actual es"+ cuenta1.getSaldo() );
+        System.out.println("Introduce la cantidad que quieres ingresar");
+        ingreso = sc.nextFloat(); sc.nextLine();
+        System.out.println("Introduce la cantidad que quieres retirar");
+        retirada = sc.nextFloat(); sc.nextLine();
+        operativa_cuenta(cuenta1,ingreso,retirada);
+    }
+    
+        public static void operativa_cuenta(CCuenta cuenta,float ingresar, float retirar) {
         try {
-            cuenta1.retirar(2300);
+            cuenta.retirar(retirar);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            cuenta.ingresar(ingresar);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
     }
-    }
+    
 }
